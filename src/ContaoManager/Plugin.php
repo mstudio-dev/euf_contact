e4513ee738f62f5d64e6d091b032a6caa90a1324
Dennis Erdmann <dennis@erdmann-freunde.de>
2018-06-08 11:48:11 +0200

    add new contao 4 structure

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * * This file is part of ErdmannFreunde/admanager.
+ *
+ * (c) 2018 Erdmann & Freunde.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    euf_contact
+ * @author     Dennis Erdmann
+ * @copyright  2018 Erdmann & Freunde
+ * @license    LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace ErdmannFreunde\ContaoContactBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ErdmannFreunde\ContaoContactBundle\ErdmannFreundeContaoContactBundle;
+
+/**
+ * Contao Manager plugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ErdmannFreundeContaoContactBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class
+                    ]
+                )
+        ];
+    }
+}
---
+++ src/ErdmannFreundeContaoContactBundle.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * * This file is part of ErdmannFreunde/euf_contact.
+ *
+ * (c) 2018 Erdmann & Freunde.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Admanager
+ * @author     Dennis Erdmann
+ * @copyright  2018 Erdmann & Freunde
+ * @license    LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace ErdmannFreunde\ContaoContactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * The Bundle class.
+ */
+class ErdmannFreundeContaoContactBundle extends Bundle
+{
+
+}
