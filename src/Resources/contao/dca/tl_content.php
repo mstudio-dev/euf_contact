23de88c4fa7f89632ebe3c3bf99bc3736082c6e9
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-06-15 11:06:39 +0200

    Do not use `addContactImage` but `addImage`

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -19,23 +19,14 @@
 /*
  * Palettes
  */
-
-$GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'addContactImage';
-
 $GLOBALS['TL_DCA']['tl_content']['palettes']['contact'] =
     '{type_legend},type,headline;'
     . '{text_legend},contact_name,contact_position,contact_email,contact_description;'
-    . '{image_legend},addContactImage;'
+    . '{image_legend},addImage;'
     . '{template_legend:hide},customTpl;'
     . '{protected_legend:hide},protected;'
     . '{expert_legend:hide},guests,cssID,space;'
     . '{invisible_legend:hide},invisible,start,stop';
-
-
-/*
- * Subpalettes
- */
-$GLOBALS['TL_DCA']['tl_content']['subpalettes']['addContactImage'] = 'singleSRC,size,overwriteMeta';
 
 
 /*
@@ -96,14 +87,4 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['contact_description'] = [
         'tl_class' => 'clr'
     ],
     'sql'       => 'mediumtext NULL'
-];
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['addContactImage'] = [
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['addContactImage'],
-    'exclude'   => true,
-    'inputType' => 'checkbox',
-    'eval'      => [
-        'submitOnChange' => true
-    ],
-    'sql'       => "char(1) NOT NULL default ''"
 ];
